57fd2fcf6eb86b703de42a547e7f3cc7d500e2a1
jdg <dgun1308@gmail.com>
2025-09-09 14:46:50 +0900

    250909 MISA Spring Boot lombok 라이브러리 추가

--- MISAdev/misa/src/main/java/com/misa/monster/dto/MonsterDTO.java
+++ MISAdev/misa/src/main/java/com/misa/monster/dto/MonsterDTO.java
@@ -1,7 +1,15 @@
 package com.misa.monster.dto;
 
+import lombok.*;
+
 import java.util.List;
 
+// lombok 어노테이션 사용
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor  // 기본 생성자
+@AllArgsConstructor // 모든 필드 포함 생성자
 public class MonsterDTO {
 
     private String monsterCode;
@@ -13,74 +21,4 @@ public class MonsterDTO {
     // 몬스터가 드랍하는 아이템 목록
     private List<MonsterDropDTO> dropList;
 
-    public MonsterDTO () {}
-
-    public MonsterDTO(String monsterCode, String monsterName, int hp, int ap, int dp, List<MonsterDropDTO> dropList) {
-        this.monsterCode = monsterCode;
-        this.monsterName = monsterName;
-        this.hp = hp;
-        this.ap = ap;
-        this.dp = dp;
-        this.dropList = dropList;
-    }
-
-    public String getMonsterCode() {
-        return monsterCode;
-    }
-
-    public void setMonsterCode(String monsterCode) {
-        this.monsterCode = monsterCode;
-    }
-
-    public String getMonsterName() {
-        return monsterName;
-    }
-
-    public void setMonsterName(String monsterName) {
-        this.monsterName = monsterName;
-    }
-
-    public int getHp() {
-        return hp;
-    }
-
-    public void setHp(int hp) {
-        this.hp = hp;
-    }
-
-    public int getAp() {
-        return ap;
-    }
-
-    public void setAp(int ap) {
-        this.ap = ap;
-    }
-
-    public int getDp() {
-        return dp;
-    }
-
-    public void setDp(int dp) {
-        this.dp = dp;
-    }
-
-    public List<MonsterDropDTO> getDropList() {
-        return dropList;
-    }
-
-    public void setDropList(List<MonsterDropDTO> dropList) {
-        this.dropList = dropList;
-    }
-
-    @Override
-    public String toString() {
-        return "MonsterDTO{" +
-                "monsterCode='" + monsterCode + '\'' +
-                ", monsterName='" + monsterName + '\'' +
-                ", hp=" + hp +
-                ", ap=" + ap +
-                ", dp=" + dp +
-                ", dropList=" + dropList +
-                '}';
-    }
 }
--- MISAdev/misa/src/main/java/com/misa/monster/dto/MonsterDropDTO.java
+++ MISAdev/misa/src/main/java/com/misa/monster/dto/MonsterDropDTO.java
@@ -1,4 +1,15 @@
 package com.misa.monster.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class MonsterDropDTO {
+
+    private String itemCode;
+    private double dropRate;
+
 }
